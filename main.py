1fe4257f69e0a6414296f234b3ec10b033338565
donghakk <kdh19583@gmail.com>
2023-05-01 11:38:11 +0900

    feat: finish main.py
    Done main.py edit

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # Test python env
 def print_hello():
-    animals = ['dog', 'cat', 'hamster']
+    animals = ['dog', 'cat', 'hamster', 'tiger'] # in oneline
     foods = [
 	'Spagetti',
-	'Pizza'
+	'Pizza',
+	'bibimbob'  # trailing comma X
     ]
     names = [
 	'John', 
 	'Jane', 
 	'Gil-dong',
+	'Dong-eun',    #trailing comma O
     ]
     for f_name in names:
         print(f'hello, {f_name}')
